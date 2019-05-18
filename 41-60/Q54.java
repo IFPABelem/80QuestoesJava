4e092103547f45f34d43e0ead6f4aaa19cff9a45
TiagoDanin <TiagoDanin@outlook.com>
2019-05-18 18:19:46 -0300

    Regra: n > 20 ? +8 || n < 20 ? -5

---
+++ 41-60/Q54.java
@@ -0,0 +1,24 @@
+import javax.swing.JOptionPane;
+
+public class Q54 {
+	/**
+		54. Faça um programa que leia 2 inteiros e efetue e a adição. Caso o valor somado seja
+		maior que 20, este deverá ser imprimido somando-se a 8, caso seja menor do que 20 o mesmo
+		deverá ser imprimido subtraindo-se 5.
+	*/
+
+	public static void main (String[] args) {
+		double a = Double.parseDouble(JOptionPane.showInputDialog("Número A: "));
+		double b = Double.parseDouble(JOptionPane.showInputDialog("Número B: "));
+
+		double numero = a + b;
+		if (numero > 20) {
+			numero += 8;
+		} else if (numero < 20) {
+			numero -= 5;
+		}
+
+		String text = "Resultado: " + numero;
+		JOptionPane.showMessageDialog(null, text);
+	}
+}
