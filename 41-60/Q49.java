9e6e587565f27051534edcfe5f92b740e0243eaa
TiagoDanin <TiagoDanin@outlook.com>
2019-05-18 17:14:57 -0300

    Metade de número maior que 20

---
+++ 41-60/Q49.java
@@ -0,0 +1,18 @@
+import javax.swing.JOptionPane;
+
+public class Q49 {
+	/**
+		Faça um programa que leia um número se ele for maior que 20, então imprima sua metade.
+	*/
+
+	public static void main (String[] args) {
+		double n = Double.parseDouble(JOptionPane.showInputDialog("Número: "));
+		String text = "Número: " + n + "\n";
+
+		if (n > 20) {
+			text += "Sua metade é " + (n / 2);
+		}
+
+		JOptionPane.showMessageDialog(null, text);
+	}
+}
