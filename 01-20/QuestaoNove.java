8f628a349c24e5768ac659df97845db157e67484
LeandroRC58 <48626897+LeandroRC58@users.noreply.github.com>
2019-05-28 14:07:48 -0300

    Update QuestaoNove.java

--- 01-20/QuestaoNove.java
+++ 01-20/QuestaoNove.java
@@ -1,12 +1,13 @@
-//9. Faça um programa que entre com dois números inteiros e exiba na tela a seguintes saída: dividendo, divisor, quociente e resto.
+/*9. Faça um programa que entre com dois números inteiros e exiba na tela a seguintes
+saída: dividendo, divisor, quociente e resto.*/
 package trabalhodejava;
     import javax.swing.JOptionPane;
     public class QuestaoNove
     {
         public static void main(String [] args)
         {
-        int a=Integer.parseInt(JOptionPane.showInputDialog("Digite o primeiro número inteiro: "));
-        int b=Integer.parseInt(JOptionPane.showInputDialog("Digite o segundo número inteiro: "));
+        int a=Integer.parseInt(JOptionPane.showInputDialog("Digite o dividendo inteiro: "));
+        int b=Integer.parseInt(JOptionPane.showInputDialog("Digite o divisor inteiro: "));
         JOptionPane.showMessageDialog(null,"Dividendo: "+a+"\n"+"Divisor: "+b+"\n"+"Quociente: "+(a/b)+
         "\n"+"Resto: "+(a%b));
         }
