23802f28948804eae726b5da1507984e2d535b39
Thiago <thiago25pro@gmail.com>
2019-05-28 13:36:43 -0300

    Update MinutosPassaram38.java

--- 21-40/MinutosPassaram38.java
+++ 21-40/MinutosPassaram38.java
@@ -1,7 +1,7 @@
 /*
 38. Faça um programa que leia um valor de hora
 informe quantos minutos se passaram desde o início do dia.
- */
+*/
 package vale4pontos;
 
 import javax.swing.JOptionPane;
@@ -10,11 +10,10 @@ public class MinutosPassaram38
 {
     public static void main(String[] args)
     {
-        double hora_do_dia = Double.parseDouble(JOptionPane.showInputDialog("Digite a hora sem os minutos: "));
-        double minutos_do_dia = Double.parseDouble(JOptionPane.showInputDialog("Digite os minutos: "));
+        double hora_do_dia = Double.parseDouble(JOptionPane.showInputDialog("Digite um valor de hora: "));
         
         double minutos_da_hora = hora_do_dia * 60;
         
-        JOptionPane.showMessageDialog(null, "Se passaram " + (minutos_da_hora + minutos_do_dia) + " minutos");
+        JOptionPane.showMessageDialog(null, "Se passaram " + minutos_da_hora + " minutos");
     }
 }
