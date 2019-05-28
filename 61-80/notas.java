9828a6bf571039af7c9f6ce2a4cf3ac165cea39b
Ewerton Belo <ewerton.bello1212@gmail.com>
2019-05-28 16:23:55 -0300

    Adicionando diferentes versões de questão

---
+++ 61-80/notas.java
@@ -0,0 +1,46 @@
+import javax.swing.JOptionPane;
+
+public class IfExXV {
+
+    public static void main(String[] args) {
+
+//        double numero = Double.parseDouble(JOptionPane.showInputDialog("Número: "));
+//        String text = "Divisível por ";
+//
+//        if (numero%2 != 0 && numero%5 !=0 && numero%10 !=0) {
+//            text += "por nenhum destes.";
+//        }else {
+//            if (numero % 10 == 0) {
+//                text += " 10 ";
+//            }
+//            if (numero % 5 == 0) {
+//                text += " 5 ";
+//            }
+//            if (numero % 2 == 0) {
+//                text += " 2 ";
+//            }
+//        }
+//        JOptionPane.showMessageDialog(null, text);
+
+        //65 - Faça um programa que leia 3 números e imprima-os em ordem decrescente
+        double n1 = Double.parseDouble(JOptionPane.showInputDialog("Insira o primeiro número"));
+        double n2 = Double.parseDouble(JOptionPane.showInputDialog("Insira o segundo número"));
+        double n3 = Double.parseDouble(JOptionPane.showInputDialog("Insira o terceiro número"));
+        
+        double[] numeros = {n1, n2, n3};
+        n2+=n1+n3;
+//        {+312, +321, 132, +123, +213, +231}
+        for (double numero : numeros) {
+            if(numero>n3){
+                n3=numero;
+            }else if(numero<n1){
+                n1=numero;
+            }
+        }
+        
+        n2-=n1+n3;
+        
+        JOptionPane.showMessageDialog(null, n3+"\n"+n2+"\n"+n1);
+
+    }
+}
