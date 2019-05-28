721d5e9db54c1c891e90d4d6da87ae30bebe8ed3
LeandroRC58 <48626897+LeandroRC58@users.noreply.github.com>
2019-05-28 14:08:39 -0300

    Update QuestaoDez.java

--- 01-20/QuestaoDez.java
+++ 01-20/QuestaoDez.java
@@ -1,15 +1,16 @@
-//10. Faça um programa que entre com 4 números e exiba na tela a média ponderada, sabendo que seus respectivos pesos são: 1, 2, 3, 4.
+/*10. Faça um programa que entre com 4 números e exiba na tela a média ponderada, sabendo que
+seus respectivos pesos são: 1, 2, 3, 4.*/
 package trabalhodejava;
     import javax.swing.JOptionPane;
     public class QuestaoDez
     {
         public static void main(String [] args)
         {
-        int n1=Integer.parseInt(JOptionPane.showInputDialog("Digite o primeiro número: ")); 
-        int n2=Integer.parseInt(JOptionPane.showInputDialog("Digite o Segundo número: "));
-        int n3=Integer.parseInt(JOptionPane.showInputDialog("Digite o Terceiro número: "));
-        int n4=Integer.parseInt(JOptionPane.showInputDialog("Digite o Quarto número: "));
-        int mediaponderada=((1*n1)+(2*n2)+(3*n3)+(4*n4))/10; 
+        float n1=Float.parseFloat(JOptionPane.showInputDialog("Digite o primeiro número: ")); 
+        float n2=Float.parseFloat(JOptionPane.showInputDialog("Digite o Segundo número: "));
+        float n3=Float.parseFloat(JOptionPane.showInputDialog("Digite o Terceiro número: "));
+        float n4=Float.parseFloat(JOptionPane.showInputDialog("Digite o Quarto número: "));
+        float mediaponderada=((1*n1)+(2*n2)+(3*n3)+(4*n4))/10; 
         JOptionPane.showMessageDialog(null,"Média ponderada: "+mediaponderada);
         }
     }
